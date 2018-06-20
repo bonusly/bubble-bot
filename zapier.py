174ece5482e150428309c09d7154934f4e187bf1
Allan McLelland <allan@bonus.ly>
2018-06-19 19:36:47 -0600

    Cleanup

--- zapier.py
+++ zapier.py
@@ -5,11 +5,12 @@ import socket
 import argparse
 import time
 
-# Set target IP, port and command to send
-ip = # Your IP goes here! (string)
-port = 9999 # default for TP-Link plugs
-duration = 5 # How many seconds bubble blower will be on, adjust as necessary
-version = 0.1
+ip =       # Your IP goes here! (string)
+port =     9999 # default for TP-Link plugs
+duration = 5 # How many seconds bubble blower will be on (adjust as necessary)
+version =  0.1
+on =       '{"system":{"set_relay_state":{"state":1}}}'
+off =      '{"system":{"set_relay_state":{"state":0}}}'
 
 # Encryption and Decryption of TP-Link Smart Home Protocol
 # XOR Autokey Cipher with starting key = 171
@@ -31,34 +32,22 @@ def decrypt(string):
 		result += chr(a)
 	return result
 
-cmd = '{"system":{"set_relay_state":{"state":1}}}'
+def send_command(cmd):
+	# Send command and receive reply
+	try:
+		sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock_tcp.connect((ip, port))
+		sock_tcp.send(encrypt(cmd))
+		data = sock_tcp.recv(2048)
+		sock_tcp.close()
 
-# Send command and receive reply
-try:
-	sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock_tcp.connect((ip, port))
-	sock_tcp.send(encrypt(cmd))
-	data = sock_tcp.recv(2048)
-	sock_tcp.close()
-
-	print "Sent:     ", cmd
-	print "Received: ", decrypt(data[4:])
-except socket.error:
-	quit("Cound not connect to host " + ip + ":" + str(port))
+		print "Sent:     ", cmd
+		print "Received: ", decrypt(data[4:])
+	except socket.error:
+		quit("Cound not connect to host " + ip + ":" + str(port))
 
 
+# Turn bubbles on, wait duration, turn bubbles off
+send_command(on)
 time.sleep(duration)
-cmd = '{"system":{"set_relay_state":{"state":0}}}'
-
-# Send command and receive reply
-try:
-	sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock_tcp.connect((ip, port))
-	sock_tcp.send(encrypt(cmd))
-	data = sock_tcp.recv(2048)
-	sock_tcp.close()
-
-	print "Sent:     ", cmd
-	print "Received: ", decrypt(data[4:])
-except socket.error:
-	quit("Cound not connect to host " + ip + ":" + str(port))
+send_command(off)

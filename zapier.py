5348f6bed4eeaea79a7ab9d4406faabfb8440050
Allan McLelland <allan@bonus.ly>
2018-06-25 11:18:20 -0600

    Add v2 encryption for newer models

--- zapier.py
+++ zapier.py
@@ -7,23 +7,35 @@ import time
 
 # You will need to forward the correct port
 # Can also use ngrok: ./ngrok tcp <localip>:9999
-ip =       # Your IP goes here! (string)
+ip =       ''   # Your IP goes here! (string)
 port =     9999 # default for TP-Link plugs
-duration = 5 # How many seconds bubble blower will be on (adjust as necessary)
-version =  0.1
+duration = 5    # How many seconds bubble blower will be on (adjust as necessary)
+version =  1    # If your plug doesn't work with v1, try v2
+
 on =       '{"system":{"set_relay_state":{"state":1}}}'
 off =      '{"system":{"set_relay_state":{"state":0}}}'
 
 # Encryption and Decryption of TP-Link Smart Home Protocol
 # XOR Autokey Cipher with starting key = 171
 def encrypt(string):
-	key = 171
-	result = "\0\0\0\0"
-	for i in string:
-		a = key ^ ord(i)
-		key = a
-		result += chr(a)
-	return result
+    key = 171
+    result = "\0\0\0"+chr(len(string))
+    for i in string:
+            a = key ^ ord(i)
+            key = a
+            result += chr(a)
+    return result
+
+# Updated encryption comes from
+# https://github.com/softScheck/tplink-smartplug/issues/22
+def encrypt_v2(string):
+    key = 171
+    result = "\0\0\0"+chr(len(string))
+    for i in string:
+            a = key ^ ord(i)
+            key = a
+            result += chr(a)
+    return result
 
 def decrypt(string):
 	key = 171
@@ -39,7 +51,10 @@ def send_command(cmd):
 	try:
 		sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock_tcp.connect((ip, port))
-		sock_tcp.send(encrypt(cmd))
+		if version == 2:
+			sock_tcp.send(encrypt_v2(cmd))
+		else:
+			sock_tcp.send(encrypt(cmd))
 		data = sock_tcp.recv(2048)
 		sock_tcp.close()
 
